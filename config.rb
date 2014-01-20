06bc7386eea7076f1d32c03986d10e32ae32306a
david calcus <targoo@davids-MacBook-Pro-2.local>
2014-01-20 21:49:36 +0000

    clean

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+# http://sass-lang.com/documentation/file.SASS_REFERENCE.html#output_style
+output_style = :expanded
